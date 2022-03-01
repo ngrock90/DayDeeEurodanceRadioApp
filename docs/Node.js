6c168d4e88d7225f39cf6772870ce4c7b19ec695
C.O.D Lab <43019988+ngrock90@users.noreply.github.com>
2022-03-01 18:59:16 +0100

    Update Node.js

--- docs/Node.js
+++ docs/Node.js
@@ -2,4 +2,4 @@ const bot = new ViberBot({
 authToken: 4ed1542af1a7e274-2a17b9ed9dedcd87-5c8aec194e3cddac, 	
 name: "DayDeeEurodanceRadio", 	
 avatar: "http://viber.com/avatar.jpg" // It is recommended to be 720x720, and no more than 100kb. }); 
-bot.setWebhook('https://ngrock90.github.io/DaydeeEurodanceRadioApp', true);
+bot.setWebhook('https://ngrock90.github.io/DayDeeEurodanceRadioApp', true);
